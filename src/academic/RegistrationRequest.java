52e83359348dc3edd86ab6b822f0650dd6ab3f57
Islam <ikabden04@gmail.com>
2026-05-17 19:15:27 +0500

    Regisstration for course fixed

--- src/academic/RegistrationRequest.java
+++ src/academic/RegistrationRequest.java
@@ -5,12 +5,15 @@ import users.Student;
 import java.io.Serializable;
 
 public class RegistrationRequest implements Serializable {
+    private int id;
     private Student student;
     private Course course;
+    private static int ID = 0;
 
     public RegistrationRequest(Student student, Course course){
         this.student = student;
         this.course = course;
+        this.id = ID++;
     }
 
     public Student getStudent(){
@@ -21,5 +24,7 @@ public class RegistrationRequest implements Serializable {
         return course;
     }
 
-
+    public int getId() {
+        return this.id;
+    }
 }
